605e6b2fdb347b13d54d2afe9c766a1441db446c
vikiv5 <vi_kov@ukr.net>
2023-03-25 18:43:41 +0200

    searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+import fetchImgApi from "./Api";
+import Notiflix from "notiflix";
+import Searchbar from "./Searchbar/Searchbar";
+
+
+
+export default class App extends Component { 
+  state = { 
+    searchQuery:"" , 
+    items : [],
+    isLoading: false,
+    page:1 ,
+    totalhits : 200,
+
+  }
+
+
+ async componentDidUpdate (_, prevState) {
+  ///const { searchQuery, page } = this.state;
+
+if (prevState.searchQuery !==this.state.searchQuery||
+  this.state.page !== prevState.page ) {
+    try{
+    this.setState({isLoading:true}) ;
+    const data = await fetchImgApi(this.state.searchQuery, this.state.page)
+    
+  this.setState (state =>  ({
+    items:[...state.items, ...data.hits]
+  }));
+  }
+catch (error) {
+  this.setState({error:true,isLoading:false})
+  }
+
+ finally{
+  this.setState({isLoading:false})
+ }}}
+
+ render()
+ {
+  const {handleSearch}=this;
+  const {items, isLoading,totalhits,error} = this.state;
+  return(
+    <>
+    <Searchbar handleSearch={handleSearch}/>
+</>
+  )}}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from "react";
+import PropTypes from 'prop-types'
+
+import {
+    SearchbarStyles,
+    SearchForm,
+    SearchFormButton,
+    SearchFormInput,
+  } from './Searchbar.styled';
+
+
+export default class Searchbar extends Component {
+    state = {
+        value:"" ,
+    };
+
+    handleInputChange = e => {
+        this.setState({ value: e.currentTarget.value.toLowerCase().trim() });
+      };
+
+      handleSubmit = e => {
+        e.preventDefault();
+        this.props.handleSearch(this.state.value);
+        this.reset();
+      }
+
+      render() {
+    const {value } = this.state;
+
+    return (
+        <SearchbarStyles>
+          <SearchForm onSubmit={this.handleSubmit}>
+            <SearchFormButton type="submit" aria-label="Search Button">
+            
+            </SearchFormButton>
+  
+            <SearchFormInput
+              onChange={this.handleInputChange}
+              value={value}
+              type="text"
+              placeholder="Search images and photos"
+            />
+          </SearchForm>
+        </SearchbarStyles>
+      );
+    }
+  }
